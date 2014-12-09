a906667f046923ded5ae72696e38d1dd19e8e7fd
HorseMD <alightedness@gmail.com>
2014-12-09 22:06:09 +0000

    Tidy up, refactoring.

--- lib/flairbot.rb
+++ lib/flairbot.rb
@@ -19,30 +19,33 @@ class Flairbot
   # Otherwise if its subject is "stop", and the user is the maintainer of this bot,
   # then we will stop the bot.
   def poll_messages valid_flairs
-    latest_messages = @client.messages({:category => :unread})
-    latest_messages.each do |message|
-      if message.is_a? RedditKit::PrivateMessage
-        @client.mark_as_read message
+    unread_messages = @client.messages({:category => :unread})
+    unread_messages.each do |message|
+      parse_message(message) if message.is_a? RedditKit::PrivateMessage
+    end
+  end
 
-        if message.subject.downcase.eql? "flair"
-          result = update_flair(message.author, message.body, valid_flairs)
+  # Parse a single message for flair updating etc...
+  def parse_message message
+    @client.mark_as_read message
 
-          if result.is_a? FlairFail
-            @client.send_message(get_error_message(message.author, result), message.author, :subject => "There was an error with setting your flair.")
-            puts "Error: #{result.message}"
-          else
-            @client.send_message(get_message(message.author, message.body), message.author, :subject => "Flair updated.")
-            puts "Success! Flair for #{message.author} set to #{message.body}."
-          end
-        elsif message.subject.downcase.eql? "stop" and message.author == @maintainer
-          puts "Received stop message from #{message.author}."
-          sign_out
-          exit
-        else
-          @client.send_message(get_unexpected_message(message.author), message.author, :subject => "re: #{message.subject}")
-          puts "Recieved unexpected message: #{message.body} from #{message.author}."
-        end
+    if message.subject.downcase.eql? "flair"
+      result = update_flair(message.author, message.body, valid_flairs)
+
+      if result.is_a? FlairFail
+        @client.send_message(get_error_message(message.author, result), message.author, :subject => "There was an error with setting your flair.")
+        puts "Error: #{result.message}"
+      else
+        @client.send_message(get_message(message.author, message.body), message.author, :subject => "Flair updated.")
+        puts "Success! Flair for #{message.author} set to #{message.body}."
       end
+    elsif message.subject.downcase.eql? "stop" and message.author == @maintainer
+      puts "Received stop message from #{message.author}."
+      sign_out
+      exit
+    else
+      @client.send_message(get_unexpected_message(message.author), message.author, :subject => "re: #{message.subject}")
+      puts "Recieved unexpected message: #{message.body} from #{message.author}."
     end
   end
 
@@ -52,39 +55,39 @@ class Flairbot
   end
 
   private
-    # Get the pretty success message for the given author.
-    def get_message author, flair
-      "Hello #{author},\n\nI just wanted to let you know that I have completed your request to use the flair \"#{flair}\", "\
-      "and your flair should now be updated.\n\nYou may need to refresh your browser cache before you see any changes: "\
-      "This is usually done by pressing the Shift key while refreshing."
-    end
+  # Get the pretty success message for the given author.
+  def get_message author, flair
+    "Hello #{author},\n\nI just wanted to let you know that I have completed your request to use the flair \"#{flair}\", "\
+    "and your flair should now be updated.\n\nYou may need to refresh your browser cache before you see any changes: "\
+    "This is usually done by pressing the Shift key while refreshing."
+  end
 
-    # Get the pretty error message for the given author, with the given error.
-    def get_error_message author, error
-      "Hello #{author},\n\nUnfortunately there was an error when processing your flair request:\n"\
-        " >#{error.message}.\n\nPlease wait a little while, and try again later."\
-        " If the problem persists, please contact a moderator and copy this message to them."
-    end
+  # Get the pretty error message for the given author, with the given error.
+  def get_error_message author, error
+    "Hello #{author},\n\nUnfortunately there was an error when processing your flair request:\n"\
+    " >#{error.message}.\n\nPlease wait a little while, and try again later."\
+    " If the problem persists, please contact a moderator and copy this message to them."
+  end
 
-    # When the bot gets a message it's not expecting, let the sender of the message know that this is
-    # actually a robot and that they should contact the maintainer.
-    def get_unexpected_message author
-      "Hello #{author},\n\nJust so you know, I am a robot and have no idea what your message means. If you have any queries, "\
-      "please contact my maintainer, [#{@maintainer}](http://reddit.com/u/#{maintainer})."
-    end
+  # When the bot gets a message it's not expecting, let the sender of the message know that this is
+  # actually a robot and that they should contact the maintainer.
+  def get_unexpected_message author
+    "Hello #{author},\n\nJust so you know, I am a robot and have no idea what your message means. If you have any queries, "\
+    "please contact my maintainer, [#{@maintainer}](http://reddit.com/u/#{maintainer})."
+  end
 
-    # Sets the given user's flair to the given flair, by classname.
-    def update_flair user, flair, valid_flairs
-      return FlairFail.new "#{flair} is not an existing flair" unless valid_flairs.include? flair
+  # Sets the given user's flair to the given flair, by classname.
+  def update_flair user, flair, valid_flairs
+    return FlairFail.new "#{flair} is not an existing flair" unless valid_flairs.include? flair
 
-      begin
-        @client.set_flair({ :subreddit => @subreddit, :css_class => flair, :user => user })
-      rescue RedditKit::TooManyClassNames
-        FlairFail.new "There are too many flairs!"
-      rescue Exception => e
-        FlairFail.new e.message
-      end
+    begin
+      @client.set_flair({ :subreddit => @subreddit, :css_class => flair, :user => user })
+    rescue RedditKit::TooManyClassNames
+      FlairFail.new "There are too many flairs!"
+    rescue Exception => e
+      FlairFail.new e.message
     end
+  end
 end
 
 class FlairFail < StandardError
