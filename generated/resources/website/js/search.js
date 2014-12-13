ba605f310e173ea28de4979c186e25b4b4ee0c42
HorseMD <alightedness@gmail.com>
2014-12-13 09:13:25 +0000

    Tooltips!

--- generated/resources/website/js/search.js
+++ generated/resources/website/js/search.js
@@ -10,4 +10,30 @@ function updateFlairList() {
     }
 }
 
+// when the user hovers over a flair, it should display a tooltip with the name.
+function onFlairHover(flair, event) {
+    var tt = document.getElementById("tooltip");
+    tt.innerHTML = names[flair.id];
+
+    var targetX = event.clientX - tt.clientWidth / 2;
+    var targetY = event.clientY + window.pageYOffset + 5;
+
+    tt.style.display = "inline-block";
+    tt.style.left = targetX + "px";
+    tt.style.top  = targetY + "px";
+}
+
+// when the user isn't mousing-over a flair, the tooltip should be invisible.
+function onMouseAway() {
+    var tt = document.getElementById("tooltip");
+    tt.style.display = "none";
+}
+
+document.getElementById("flairs").addEventListener("mousemove", function(event) {
+    if(event.target.tagName.toLowerCase() == 'li') {
+        onFlairHover(event.target, event);
+    } else {
+        onMouseAway();
+    }
+});
 document.getElementById("search").addEventListener("input", updateFlairList, false);
